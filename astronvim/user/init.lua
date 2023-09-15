ce4d4c5991f031aefc3f74521bcf351c5975e078
sonu405 <godson0343@gmail.com>
2023-09-15 12:54:13 +0500

    removed tabs

--- astronvim/user/init.lua
+++ astronvim/user/init.lua
@@ -1,3 +1,17 @@
 return {
-  colorscheme = 'ayu'
+  colorscheme = 'ayu',
+  options = {
+    opt = {
+      showtabline = 0, -- don't show tabline
+    },
+  },
+  mappings = {
+    n = { -- disable <leader>b mappings
+      ["<leader>b"] = false,
+      ["<leader>bb"] = false,
+      ["<leader>bd"] = false,
+      ["<leader>b\\"] = false,
+      ["<leader>b|"] = false,
+    },
+  },
 }
--- astronvim/user/plugins/user.lua
+++ astronvim/user/plugins/user.lua
@@ -6,4 +6,18 @@ return {
       require('ayu').setup {}
     end,
   },
+  {
+    "ThePrimeagen/vim-be-good"
+  },
+  {
+    "akinsho/toggleterm.nvim",
+    enabled = false,
+  },
+  {
+    "rebelot/heirline.nvim",
+    opts = function(_, opts)
+      opts.tabline = nil -- remove tabline
+      return opts
+    end,
+  },
 }
